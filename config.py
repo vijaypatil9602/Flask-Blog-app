0def1f4ac88a1e1b1607bacdc575509b5688c3d7
vijaypatil9602 <vijaypatil9602@gmail.com>
2025-11-11 16:57:27 +0530

    Removed all unused email code to finalize for deployment

--- config.py
+++ config.py
@@ -13,11 +13,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'mysql+pymysql://root:root@localhost/my_flask_app'
     
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # Email configuration
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS') 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False 
